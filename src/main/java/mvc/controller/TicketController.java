5b5dec9972f154e410d9b5b6c493773eb9e05994
benweizhu <benweizhu@gmail.com>
2018-01-28 18:21:13 +0800

    add logback

--- src/main/java/mvc/controller/TicketController.java
+++ src/main/java/mvc/controller/TicketController.java
@@ -2,10 +2,13 @@ package mvc.controller;
 
 import mvc.entity.Ticket;
 import mvc.repository.TicketRepository;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @RestController
 public class TicketController {
@@ -13,8 +16,11 @@ public class TicketController {
     @Autowired
     private TicketRepository ticketRepository;
 
+    private static final Logger LOGGER = getLogger(TicketController.class);
+
     @RequestMapping(value = "/ticket/{id}")
     public Ticket getTicket(@PathVariable long id) {
+        LOGGER.error("error get ticket %d", id);
         return ticketRepository.findOne(id);
     }
 }
